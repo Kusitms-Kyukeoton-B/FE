eb515b601d7254b5c3e3526bf769bab6dc5bd5ce
HIHJH <2170095@ewhain.net>
2024-04-06 23:25:22 +0900

    feat: color system 양식

---
+++ src/styles/colors.ts
@@ -0,0 +1,9 @@
+export const colors = {
+  grey0: "#464646",
+  grey05: "#5d5d5d",
+  grey1: "#7f7f7f",
+  grey2: "#B4B4B4",
+  grey3: "#cecece",
+  grey4: "#efefef",
+  grey5: "#F4F4F4",
+} as const;
