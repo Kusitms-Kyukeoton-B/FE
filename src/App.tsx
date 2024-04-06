613941f7c9b4582c5b1dde101aeb651aec1fae9e
Seunghyo <shyo0218@naver.com>
2024-04-07 00:49:32 +0900

    feat: main page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
-import ExamplePage from "./pages/ExamplePage";
 import LoginPage from "./pages/LoginPage";
 import GridPage from "./pages/home/GridPage";
 import ImageDetailPage from "./pages/home/ImageDetailPage";
@@ -12,11 +11,10 @@ import PointPage from "./pages/mypage/PointPages";
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<ExamplePage />} />
       <Route path="/login" element={<LoginPage />} />
       <Route path="/profile" element={<MyPage />} />
       <Route path="/profile/edit" element={<MyPageEditPage />} />
-      <Route path="/ex" element={<GridPage />} />
+      <Route path="/" element={<GridPage />} />
       <Route path="/point" element={<PointPage />} />
       <Route path="/detail/:index" element={<ImageDetailPage />} />
       <Route path="/file" element={<FileUploadPage />} />
--- src/pages/home/GridPage.tsx
+++ src/pages/home/GridPage.tsx
@@ -5,6 +5,9 @@ import { Layout } from "../../styles/styles";
 import { useNavigate } from "react-router-dom";
 import ImageGrid from "../../components/GridImages";
 import Navigation from "../../components/Navigation";
+import FilterButtonComponent from "../../components/FilteringButton";
+import plusicon from "../../assets/plus_icon.svg";
+import searchicon from "../../assets/search_icon.svg";
 
 interface ImageData {
   src: string;
@@ -26,6 +29,11 @@ const GridPage = () => {
   const [images, setImages] = useState<ImageData[]>([]);
   const [page, setPage] = useState<number>(1);
   const loader = useRef(null);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchTerm(e.target.value);
+  };
 
   const handleImageClick = (index: number) => {
     nav(`/detail/${encodeURIComponent(index)}`);
@@ -73,10 +81,28 @@ const GridPage = () => {
 
   return (
     <Layout>
-      <ScrollableDiv onScroll={handleScroll}>
-        <ImageGrid images={images} onImageClick={handleImageClick} />
-        <div ref={loader} />
-      </ScrollableDiv>
+      <Container>
+        <TopContainer>
+          <TopLeft />
+          <TopRight>
+            <img src={plusicon} />
+          </TopRight>
+        </TopContainer>
+        <SearchBarContainer>
+          <SearchInput
+            type="text"
+            placeholder="Search"
+            value={searchTerm}
+            onChange={handleSearchChange}
+          />
+          <SearchIcon src={searchicon} alt="Search" />
+        </SearchBarContainer>
+        <FilterButtonComponent />
+        <ScrollableDiv onScroll={handleScroll}>
+          <ImageGrid images={images} onImageClick={handleImageClick} />
+          <div ref={loader} />
+        </ScrollableDiv>
+      </Container>
       <Navigation />
     </Layout>
   );
@@ -84,7 +110,7 @@ const GridPage = () => {
 
 const ScrollableDiv = styled.div`
   height: 100vh;
-  width: 100vw;
+  width: 90%;
   padding-left: 18px;
   padding-right: 18px;
   overflow: auto;
@@ -98,6 +124,70 @@ const ScrollableDiv = styled.div`
 
   // IE 및 Edge용 스크롤바 숨기기
   -ms-overflow-style: none;
+`;
+
+const SearchBarContainer = styled.div`
+  display: flex;
+  align-items: center;
+  width: 353px;
+  height: 46px;
+  margin-left: 18px;
+  border-radius: 30px;
+  background: #d9d9d9;
+  margin-top: 10px;
+`;
+
+const SearchInput = styled.input`
+  flex-grow: 1;
+  height: 100%;
+  padding-left: 20px;
+  padding-right: 36px; // Make room for the icon inside the search bar
+  border-radius: 30px;
+  border: none;
+  background: transparent;
+  outline: none;
+
+  &::placeholder {
+    color: #a8a8a8;
+  }
+`;
+
+const SearchIcon = styled.img`
+  width: 24px;
+  height: 24px;
+  margin-right: 10px;
+  cursor: pointer;
+`;
+
+const Container = styled.div`
+  display: flex;
+  width: 100%;
+  flex-direction: column;
+`;
+
+const TopContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin-top: 10px;
+`;
+
+const TopLeft = styled.div`
+  width: 81px;
+  height: 40px;
+  border-radius: 30px;
+  background: #6e59ef;
+  margin-left: 20px;
+`;
+
+const TopRight = styled.div`
+  width: 38px;
+  display: flex;
+  height: 38px;
+  border-radius: 50%;
+  background: #6e59ef;
+  justify-content: center;
+  align-items: center;
+  margin-right: 20px;
 `;
 
 export default GridPage;
